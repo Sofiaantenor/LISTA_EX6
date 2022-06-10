1cf230913ac85a33a363448b72b4b7e37ba10d5c
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 15:44:06 -0300

    Add files via upload

---
+++ AMS_Ex6_22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AMS_Ex6_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura = 1.90;
+            double pesoIdeal = (72.7 * altura) - 58;
+
+            Console.WriteLine("");
+            Console.WriteLine("         ***CÁLCULO DO PESO IDEAL DE UM HOMEM***      ");
+        
+            Console.WriteLine($"O peso ideal desse homem de acordo com a sua altura é {pesoIdeal} Kg");
+            Console.WriteLine("");
+        }
+    }
+}
